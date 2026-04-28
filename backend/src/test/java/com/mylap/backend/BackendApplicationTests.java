65b62e0f4a43248704d6ef6fb5b82b3d72a15c79
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-04-28 12:18:39 +0530

    added frontend and backend

---
+++ backend/src/test/java/com/mylap/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylap.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
