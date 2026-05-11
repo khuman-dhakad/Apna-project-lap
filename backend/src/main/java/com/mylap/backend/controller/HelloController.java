a91af89dbeda4290daa016cdcc6970bb3893ff40
singh.nikhil.devloper <singh.nikhil.devloper@gmail.com>
2026-05-11 13:08:13 +0530

    Connect frontend with backend

---
+++ backend/src/main/java/com/mylap/backend/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.mylap.backend.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Backend Connected Successfully!";
+    }
+}
